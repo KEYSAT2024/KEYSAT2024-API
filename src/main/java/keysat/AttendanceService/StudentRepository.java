292a49a4172b6d07684846d6a5d29f48ba9dd32d
BLewis <45450044+BLewis-Keyin@users.noreply.github.com>
2024-02-14 09:57:42 -0500

    Delete src/main/java/keysat/AttendanceService directory

--- src/main/java/keysat/AttendanceService/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package keysat.AttendanceService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
-

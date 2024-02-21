0d105c784bd19e42d36168f8bc34fc6826c7fa05
BL-Keyin <you@example.com>
2024-02-20 19:58:31 -0500

    Add lombok to LoginDTO.java

--- src/main/java/keysat/entities/LoginDTO.java
+++ src/main/java/keysat/entities/LoginDTO.java
@@ -1,22 +1,19 @@
 package keysat.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class LoginDTO {
     private String username;
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }

6b446e23c448ef74e85e22e388823eb49d3c1963
kostiantyn <kostiantyn@test.com>
2024-02-25 00:34:25 -0330

    fix: added entity and PK type

--- src/main/java/keysat/repository/InstructorRepository.java
+++ src/main/java/keysat/repository/InstructorRepository.java
@@ -1,6 +1,7 @@
 package keysat.repository;
 
+import keysat.entities.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InstructorRepository extends JpaRepository {
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
 }

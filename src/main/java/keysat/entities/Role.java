38c746488b1fe2469b51002761f68913fd676011
kostiantyn <kostiantyn@test.com>
2024-02-26 18:22:07 -0330

    fix: ugly foreign key names like 'k2hfgkfhjwev'

--- src/main/java/keysat/entities/Role.java
+++ src/main/java/keysat/entities/Role.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "role")
 @Table(name = "role")
 @Getter
 @Setter
--- src/main/java/keysat/entities/Student.java
+++ src/main/java/keysat/entities/Student.java
@@ -1,8 +1,15 @@
 package keysat.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
 @Entity(name = "student")
 @Table(name = "student")
+@PrimaryKeyJoinColumn(
+        name = "student_id",
+        referencedColumnName = "user_id",
+        foreignKey = @ForeignKey(name = "student_user_id_fkey")
+)
 public class Student extends User {}

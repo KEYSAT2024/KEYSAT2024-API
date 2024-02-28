0921a77480f1129e2fb11a395242f59056900d44
kostiantyn <kostiantyn@test.com>
2024-02-28 19:36:19 -0330

    new: moved business logic to 'CourseService'
    modified: 'CourseController', improved 'addCourse', so course can be added for specified instructor
    
    new: 'updateCourse', 'deleteCourse' methods

--- src/main/java/keysat/controller/CourseController.java
+++ src/main/java/keysat/controller/CourseController.java
@@ -1,8 +1,9 @@
 package keysat.controller;
 
-import keysat.repository.CourseRepository;
 import keysat.entities.Course;
+import keysat.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,24 +13,45 @@ import java.util.List;
 @RequestMapping("/courses")
 public class CourseController {
 
+    private final CourseService courseService;
+
     @Autowired
-    private CourseRepository courseRepository;
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping
-    public ResponseEntity<List<Course>> getAllCourses() {
-        return ResponseEntity.ok(courseRepository.findAll());
+    public List<Course> getAllCourses() {
+        return this.courseService.getAllCourses();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
-        return courseRepository.findById(id)
+        return this.courseService.getCourseById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<Course> addCourse(@RequestBody Course course) {
-        return ResponseEntity.ok(courseRepository.save(course));
+    public Course addCourse(
+            @RequestBody Course course,
+            @RequestParam(name = "instructorId", required = false) Long instructorId
+    ) {
+        return this.courseService.addCourse(course, instructorId);
     }
 
+    @PutMapping("{id}")
+    public ResponseEntity<Course> updateCourse(
+            @PathVariable Long id,
+            @RequestBody Course updatedCourse,
+            @RequestParam(name = "instructorId", required = false) Long instructorId
+    ) {
+        return this.courseService.updateCourse(id, updatedCourse, instructorId);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourse(@PathVariable Long id) {
+        this.courseService.deleteCourseById(id);
+    }
 }

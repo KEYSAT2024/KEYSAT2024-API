85b4fba9bcb35ef41ba6b32883ba420d6eab599a
BL-Keyin <you@example.com>
2024-02-28 08:14:38 -0500

    Rename API.java to TestRoutes.java On branch main Changes to be committed: 	renamed:    src/main/java/keysat/API.java -> src/main/java/keysat/TestRoutes.java

--- src/main/java/keysat/TestRoutes.java
+++ src/main/java/keysat/TestRoutes.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class API {
+public class TestRoutes {
 
     @GetMapping("/")
     public String keysat() {
